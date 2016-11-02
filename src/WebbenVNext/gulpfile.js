f221945137beb34c22182e9a2ce4bf99c256a9f8
Peter Örneholm <peter@orneholm.com>
2016-11-02 15:23:49 +0100

    Add TypeScript

---
+++ src/WebbenVNext/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+gulp.task('default', function () {
+    return gulp.src('src/**/*.ts')
+        .pipe(ts({
+            out: 'app.js'
+        }))
+        .pipe(gulp.dest('wwwroot/dist'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.ts', ['default']);
+});
---
+++ src/WebbenVNext/src/main.ts
@@ -0,0 +1,48 @@
+﻿class PreviewImage {
+    constructor(private url: string) { }
+
+    public toHtml(): string {
+        return `<div class="card">
+                    <img src="${this.url}" class="img-fluid" />
+                </div>`;
+    }
+}
+
+class FileApplication {
+    private filesInput: HTMLInputElement;
+    private filesPreviewWrapper: HTMLDivElement;
+
+    constructor() {
+        this.filesInput = document.querySelector('#files') as HTMLInputElement;
+        this.filesPreviewWrapper = document.querySelector('#files-preview') as HTMLDivElement;
+    }
+
+    public run(): void {
+        if (this.filesInput !== undefined && this.filesPreviewWrapper !== undefined) {
+            this.filesInput.addEventListener('change', event => {
+                this.previewImages();
+            });
+        }
+    }
+
+    private previewImages(): void {
+        this.filesPreviewWrapper.innerHTML = '';
+
+        let files = this.filesInput.files;
+        for (let file of Array.from(files)) {
+            this.previewImage(file);
+        }
+    }
+
+    private previewImage(file: File) {
+        let fileReader = new FileReader();
+        fileReader.addEventListener('load', () => {
+            let image = new PreviewImage(fileReader.result);
+            this.filesPreviewWrapper.innerHTML += image.toHtml();
+        });
+        fileReader.readAsDataURL(file);
+    }
+}
+
+var app = new FileApplication();
+app.run();
