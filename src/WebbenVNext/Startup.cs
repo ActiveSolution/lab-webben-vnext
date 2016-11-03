3219d552df30ef39d4f687bcd1029f96a7c1646d
Peter Örneholm <peter@orneholm.com>
2016-11-03 23:23:41 +0100

    Remove Azure-stuff

--- src/WebbenVNext/Startup.cs
+++ src/WebbenVNext/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WebbenVNext.Storage;
@@ -9,45 +8,17 @@ namespace WebbenVNext
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddEnvironmentVariables();
-
-            if (env.IsDevelopment())
-            {
-                builder.AddUserSecrets();
-            }
-
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddOptions();
 
             // Local blobs
-            //services.AddTransient<IBlobs, LocalBlobs>();
-
-            // Azure blobs
-            services.AddTransient<IBlobs, AzureBlobs>();
-            services.Configure<AzureBlobsOptions>(options =>
-            {
-                options.StorageConnectionString = Configuration.GetValue<string>("AzureBlobConnectionString");
-            });
+            services.AddTransient<IBlobs, LocalBlobs>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
