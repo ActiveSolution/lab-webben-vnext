c157876414cc1408123a38bd1c3ec9506844899e
Jakob Ehn <jakob@ehn.nu>
2017-06-03 11:21:14 +0200

    Uppdaterat med kod för lab2

--- src/WebbenVNext/Startup.cs
+++ src/WebbenVNext/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WebbenVNext.Storage;
@@ -8,13 +9,42 @@ namespace WebbenVNext
 {
     public class Startup
     {
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+            else
+            {
+                builder.AddJsonFile($"appsettings.json", optional: true);
+            }
+
+            Configuration = builder.Build();
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddOptions();
 
             // Local blobs
-            services.AddTransient<IBlobs, LocalBlobs>();
+            services.AddTransient<IBlobs, AzureBlobs>();
+
+            var connString = Configuration.GetValue<string>("AzureBlobConnectionString");
+            services.Configure<AzureBlobsOptions>(options =>
+            {
+                options.StorageConnectionString = connString;
+            });
+
 
         }
 
