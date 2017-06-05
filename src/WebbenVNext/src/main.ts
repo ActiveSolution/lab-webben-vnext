b8389f16f7ddbb9648a8ce2e55af8bb025560af3
Peter Örneholm <peter@orneholm.com>
2017-06-05 08:18:39 +0200

    Enable preview for image uploads

--- src/WebbenVNext/src/main.ts
+++ src/WebbenVNext/src/main.ts
@@ -1,3 +1,49 @@
-﻿let userName: string;
-userName = prompt('What your name?');
-alert(`Hello ${userName}!`);
+﻿class PreviewImage {
+    constructor(private url: string) { }
+
+    public toHtml(): string {
+        return `<div class="card">
+                <img src="${this.url}" class="img-fluid" />
+            </div>`;
+    }
+}
+
+
+class FileApplication {
+    private filesInput: HTMLInputElement;
+    private filesPreviewWrapper: HTMLDivElement;
+
+    constructor() {
+        this.filesInput = document.querySelector('#files') as HTMLInputElement;
+        this.filesPreviewWrapper = document.querySelector('#files-preview') as HTMLDivElement;
+    }
+
+    public run(): void {
+        if (this.filesInput !== undefined && this.filesPreviewWrapper !== undefined) {
+            this.filesInput.addEventListener('change', event => {
+                this.previewImages();
+            });
+        }
+    }
+
+    private previewImages(): void {
+        this.filesPreviewWrapper.innerHTML = '';
+
+        let files = this.filesInput.files;
+        for (let i = 0; i < files.length; i++) {
+            this.previewImage(files[0]);
+        }
+    }
+
+    private previewImage(file: File) {
+        let fileReader = new FileReader();
+        fileReader.addEventListener('load', () => {
+            let image = new PreviewImage(fileReader.result);
+            this.filesPreviewWrapper.innerHTML += image.toHtml();
+        });
+        fileReader.readAsDataURL(file);
+    }
+}
+
+var app = new FileApplication();
+app.run();
