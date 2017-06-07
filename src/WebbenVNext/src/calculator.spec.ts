a37314a7e908fb9b229e9f765deedc18a4b94fc2
Peter Örneholm <peter@orneholm.com>
2017-06-07 11:49:49 +0200

    Add frontend unit tests

---
+++ src/WebbenVNext/src/calculator.spec.ts
@@ -0,0 +1,19 @@
+﻿import { Calculator } from './calculator';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('Calculator', () => {
+    it('should add numbers', () => {
+        const calc = new Calculator();
+        const result = calc.add(10, 20);
+
+        expect(result).to.equal(30);
+    });
+
+    it('should multiply numbers', () => {
+        const calc = new Calculator();
+        const result = calc.multiply(10, 20);
+
+        expect(result).to.equal(200);
+    });
+});
---
+++ src/WebbenVNext/src/calculator.ts
@@ -0,0 +1,11 @@
+﻿export class Calculator {
+    constructor() { }
+
+    public add(x: number, y: number): number {
+        return x + y
+    }
+
+    public multiply(x: number, y: number): number {
+        return x * y
+    }
+}
